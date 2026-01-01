cd4be227cd9bb7140ed7859a280f1dda49d3b6ed
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-01-01 08:59:19 +0530

    Create Plus_One.cpp

---
+++ Plus_One.cpp
@@ -0,0 +1,24 @@
+// 66.  Plus One 
+
+class Solution {
+public:
+    std::vector<int> plusOne(std::vector<int>& digits) {
+        int n = digits.size();
+
+        
+        for (int i = n - 1; i >= 0; --i) {
+            
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            
+            else {
+                digits[i] = 0;
+            }
+        }
+
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
