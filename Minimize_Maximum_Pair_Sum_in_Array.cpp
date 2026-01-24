1885552c0fdad1c916a6556b0798f354100170e6
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-01-24 21:54:26 +0530

    Create Minimize_Maximum_Pair_Sum_in_Array.cpp

---
+++ Minimize_Maximum_Pair_Sum_in_Array.cpp
@@ -0,0 +1,15 @@
+// 1877. Minimize Maximum Pair Sum in Array
+
+class Solution {
+ public:
+  int minPairSum(vector<int>& nums) {
+    int ans = 0;
+
+    ranges::sort(nums);
+
+    for (int i = 0, j = nums.size() - 1; i < j;)
+      ans = max(ans, nums[i++] + nums[j--]);
+
+    return ans;
+  }
+};
