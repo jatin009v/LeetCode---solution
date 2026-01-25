56ad586fa6b7212e7d94d55a8daad71069de652b
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-01-25 10:56:41 +0530

    Create Minimum Difference Between Highest and Lowest of K Scores.cpp

---
+++ Minimum Difference Between Highest and Lowest of K Scores.cpp
@@ -0,0 +1,15 @@
+// 1984. Minimum Difference Between Highest and Lowest of K Scores
+
+class Solution {
+ public:
+  int minimumDifference(vector<int>& nums, int k) {
+    ranges::sort(nums);
+
+    int ans = nums[k - 1] - nums[0];
+
+    for (int i = k; i < nums.size(); ++i)
+      ans = min(ans, nums[i] - nums[i - k + 1]);
+
+    return ans;
+  }
+};
