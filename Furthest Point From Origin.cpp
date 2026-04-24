b818a68e6f3803c21802eb55314623c2ee007e7b
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-04-24 18:34:47 +0530

    Create Furthest Point From Origin.cpp

---
+++ Furthest Point From Origin.cpp
@@ -0,0 +1,20 @@
+// 2833. Furthest Point From Origin
+
+class Solution {
+ public:
+  int furthestDistanceFromOrigin(string moves) {
+    int countL = 0;
+    int countR = 0;
+    int countUnderline = 0;
+
+    for (const char c : moves)
+      if (c == 'L')
+        ++countL;
+      else if (c == 'R')
+        ++countR;
+      else  // c == '_'
+        ++countUnderline;
+
+    return abs(countL - countR) + countUnderline;
+  }
+};
