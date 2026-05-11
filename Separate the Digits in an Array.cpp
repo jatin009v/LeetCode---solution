58d67a2bec7af3b04514cd3d29b67f2028c49cb2
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-05-11 07:33:21 +0530

    Create Separate the Digits in an Array.cpp

---
+++ Separate the Digits in an Array.cpp
@@ -0,0 +1,14 @@
+// 2553. Separate the Digits in an Array
+
+class Solution {
+ public:
+  vector<int> separateDigits(vector<int>& nums) {
+    vector<int> ans;
+
+    for (const int num : nums)
+      for (const char c : to_string(num))
+        ans.push_back(c - '0');
+
+    return ans;
+  }
+};
