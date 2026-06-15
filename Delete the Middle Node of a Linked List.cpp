84a7feca7fb619c3bb325dc6664aa1d70a3acda3
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-06-15 08:22:27 +0530

    Create Delete the Middle Node of a Linked List.cpp

---
+++ Delete the Middle Node of a Linked List.cpp
@@ -0,0 +1,19 @@
+// 2095. Delete the Middle Node of a Linked List
+
+class Solution {
+ public:
+  ListNode* deleteMiddle(ListNode* head) {
+    ListNode dummy(0, head);
+    ListNode* slow = &dummy;
+    ListNode* fast = &dummy;
+
+    while (fast->next != nullptr && fast->next->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+
+    // Delete the middle node.
+    slow->next = slow->next->next;
+    return dummy.next;
+  }
+};
