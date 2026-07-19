024f7cee32375b47267ef0b15748dc82025a969d
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-07-19 20:28:56 +0530

    Create Smallest Subsequence of Distinct Characters.cpp

---
+++ Smallest Subsequence of Distinct Characters.cpp
@@ -0,0 +1,27 @@
+// 1081. Smallest Subsequence of Distinct Characters
+
+class Solution {
+ public:
+  string smallestSubsequence(string text) {
+    string ans;
+    vector<int> count(128);
+    vector<bool> used(128);
+
+    for (const char c : text)
+      ++count[c];
+
+    for (const char c : text) {
+      --count[c];
+      if (used[c])
+        continue;
+      while (!ans.empty() && ans.back() > c && count[ans.back()] > 0) {
+        used[ans.back()] = false;
+        ans.pop_back();
+      }
+      used[c] = true;
+      ans.push_back(c);
+    }
+
+    return ans;
+  }
+};
