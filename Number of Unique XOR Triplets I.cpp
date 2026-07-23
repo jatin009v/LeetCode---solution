3cbd23d9cc9abd24637a9d85404b2bb3ee969cd6
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-07-23 21:17:25 +0530

    Create Number of Unique XOR Triplets I.cpp

---
+++ Number of Unique XOR Triplets I.cpp
@@ -0,0 +1,10 @@
+// 3513. Number of Unique XOR Triplets I
+class Solution {
+ public:
+  int uniqueXorTriplets(vector<int>& nums) {
+    const int n = nums.size();
+    if (n < 3)
+      return n;
+    return 1 << (static_cast<int>(log2(n)) + 1);
+  }
+};
