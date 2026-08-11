57040067219d59fe618375cf5b636d458e0625b5
Jatin Gupta <128694793+jatin009v@users.noreply.github.com>
2026-08-11 08:55:04 +0530

    Create Smallest Missing Integer Greater Than Sequential Prefix Sum.cpp

---
+++ Smallest Missing Integer Greater Than Sequential Prefix Sum.cpp
@@ -0,0 +1,20 @@
+// 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum
+
+class Solution {
+ public:
+  int missingInteger(vector<int>& nums) {
+    const unordered_set<int> numsSet{nums.begin(), nums.end()};
+    int ans = nums[0];
+
+    for (int i = 1; i < nums.size(); ++i) {
+      if (nums[i] != nums[i - 1] + 1)
+        break;
+      ans += nums[i];
+    }
+
+    while (numsSet.contains(ans))
+      ++ans;
+
+    return ans;
+  }
+};
